69d67490eeb6004b97aa0a518483e44f8ef29d02
juanjose23 <jrios9836@gmail.com>
2023-06-18 02:40:05 -0600

    vista cliente terminada

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("menu", views.menu, name="menu"),
+    path("contacto", views.contacto, name="contacto"),
+    path("acerca",views.acerca,name="acerca"),
+    path("login", views.login, name="login"),
+    path("registrar", views.registrar, name="registrar")
 ]
--- orders/views.py
+++ orders/views.py
@@ -3,4 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    return render(request, "index.html")
+
+def acerca(request):
+    return render(request, "acerca.html")
+
+
+
+def menu(request):
+    return render(request, "menu.html")
+
+
+def contacto(request):
+    return render(request, "contacto.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def registrar(request):
+    return render(request, "registrar.html")
